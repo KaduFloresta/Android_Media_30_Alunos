2f6dac3ab3904deb2fc169aa428fcc3c910b3dcd
Kadu Floresta <cefloresta1@gmail.com>
2021-04-02 02:42:25 -0300

    details

--- app/src/main/java/com/example/android_media_30_alunos/DataActivity.java
+++ app/src/main/java/com/example/android_media_30_alunos/DataActivity.java
@@ -2,11 +2,16 @@ package com.example.android_media_30_alunos;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 public class DataActivity extends AppCompatActivity {
-    String[] alunoData;
+    ArrayList<Aluno> alunosNotas = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,7 +19,7 @@ public class DataActivity extends AppCompatActivity {
         setContentView(R.layout.activity_data);
 
         Bundle args = getIntent().getExtras();
-        alunoData = args.getStringArray("aluno");
+        alunosNotas = args.getParcelableArrayList("aluno");
 
         TextView tNome = (TextView) findViewById(R.id.dataNome);
         TextView tDtNasciemento = (TextView) findViewById(R.id.dataDtNascimento);
@@ -25,13 +30,37 @@ public class DataActivity extends AppCompatActivity {
         TextView tNota4 = (TextView) findViewById(R.id.dataNota4);
         TextView tMedia = (TextView) findViewById(R.id.dataMedia);
 
-        tNome.setText(alunoData[0]);
-        tDtNasciemento.setText(alunoData[2]);
-        tEndereco.setText(alunoData[1]);
-        tNota1.setText(alunoData[3]);
-        tNota2.setText(alunoData[4]);
-        tNota3.setText(alunoData[5]);
-        tNota4.setText(alunoData[6]);
-        tMedia.setText(alunoData[7]);
+        alunosNotas.forEach(aluno -> {
+            tNome.setText(aluno.nome);
+            tDtNasciemento.setText(aluno.dtNascimento);
+            tEndereco.setText(aluno.endereco);
+            tNota1.setText(aluno.notas[0]);
+            tNota2.setText(aluno.notas[1]);
+            tNota3.setText(aluno.notas[2]);
+            tNota4.setText(aluno.notas[3]);
+            tMedia.setText(aluno.media);
+        });
+
+        Button btAdd = (Button) findViewById(R.id.btnAdd);
+        btAdd.setOnClickListener(onClickAdicionar());
+    }
+
+    private View.OnClickListener onClickAdicionar() {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int length = alunosNotas.size();
+                if (length <= 30) {
+                    Intent myIntent = new Intent();
+                    Bundle params = new Bundle();
+                    params.putParcelableArrayList("aluno", alunosNotas);
+                    myIntent.putExtras(params);
+                    setResult(2, myIntent);
+                    finish();
+                } else {
+                    throw new Error("Somente 30 Alunos!");
+                }
+            }
+        };
     }
 }
